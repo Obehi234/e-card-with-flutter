6f1f9a1267c0ed892d8b495de12b4b97e551b916
obehi234 <obehi.ataga234@gmail.com>
2023-12-31 09:15:52 +0100

    Added divider widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,46 +14,52 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                          const CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/yaya.jpeg'),
-                          ),
-                          const Text('Yaya Tataki',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-                          Text('MOBILE DEVELOPER',
-                style: TextStyle(
-                    fontFamily: 'SourceSans3',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
-                    fontWeight: FontWeight.bold)),
-                          Card(
-                margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                    leading: Icon(Icons.phone, color: Colors.teal.shade900),
-                    title: Text('+44 123 456',
-                        style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'SourceSans3',
-                            fontSize: 20.0)))),
-                          Card(
-                margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                    leading: Icon(Icons.email, color: Colors.teal.shade900),
-                    title: Text('yaya@gmail.com',
-                        style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'SourceSans3',
-                            fontSize: 20.0))))
-                        ]))),
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/yaya.jpeg'),
+                ),
+                const Text('Yaya Tataki',
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text('MOBILE DEVELOPER',
+                    style: TextStyle(
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade100,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20.0,
+                  width:150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100
+                  )
+                ),
+                Card(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                        leading: Icon(Icons.phone, color: Colors.teal.shade900),
+                        title: Text('+44 123 456',
+                            style: TextStyle(
+                                color: Colors.teal.shade900,
+                                fontFamily: 'SourceSans3',
+                                fontSize: 20.0)))),
+                Card(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                        leading: Icon(Icons.email, color: Colors.teal.shade900),
+                        title: Text('yaya@gmail.com',
+                            style: TextStyle(
+                                color: Colors.teal.shade900,
+                                fontFamily: 'SourceSans3',
+                                fontSize: 20.0))))
+              ]))),
     );
   }
 }
