963b9643e6577a4538fab533d47f616fd90bcebc
obehi234 <obehi.ataga234@gmail.com>
2023-12-31 08:48:31 +0100

    Added padding class to card widget

--- lib/main.dart
+++ lib/main.dart
@@ -34,34 +34,40 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 margin: const EdgeInsets.symmetric(
                     vertical: 10.0, horizontal: 25.0),
-                child: Row(children: [
-                  const Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  const SizedBox(width: 20.0),
-                  Text('+444 123 456',
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSans3',
-                          fontSize: 20.0))
-                ])),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(children: [
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(width: 20.0),
+                    Text('+444 123 456',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSans3',
+                            fontSize: 20.0))
+                  ]),
+                )),
             Card(
                 color: Colors.white,
                 margin: const EdgeInsets.symmetric(
                     vertical: 10.0, horizontal: 25.0),
-                child: Row(children: [
-                  const Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  const SizedBox(width: 20.0),
-                  Text('yTataki@gmail.com',
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSans3',
-                          fontSize: 20.0))
-                ]))
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(children: [
+                    const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(width: 20.0),
+                    Text('yTataki@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSans3',
+                            fontSize: 20.0))
+                  ]),
+                ))
           ]))),
     );
   }
