a5f87aa4a09baa01370c25b8d73203a1f8da51b2
obehi234 <obehi.ataga234@gmail.com>
2023-12-31 09:11:20 +0100

    Replaced row with listTile and centered column

--- lib/main.dart
+++ lib/main.dart
@@ -13,62 +13,47 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-              child: Column(children: [
-            const CircleAvatar(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                          const CircleAvatar(
               radius: 50.0,
               backgroundImage: AssetImage('images/yaya.jpeg'),
-            ),
-            const Text('Yaya Tataki',
+                          ),
+                          const Text('Yaya Tataki',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold)),
-            Text('MOBILE DEVELOPER',
+                          Text('MOBILE DEVELOPER',
                 style: TextStyle(
                     fontFamily: 'SourceSans3',
                     fontSize: 20.0,
                     color: Colors.teal.shade100,
                     fontWeight: FontWeight.bold)),
-            Card(
-                color: Colors.white,
+                          Card(
                 margin: const EdgeInsets.symmetric(
                     vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(children: [
-                    const Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    const SizedBox(width: 20.0),
-                    Text('+444 123 456',
+                child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal.shade900),
+                    title: Text('+44 123 456',
                         style: TextStyle(
                             color: Colors.teal.shade900,
                             fontFamily: 'SourceSans3',
-                            fontSize: 20.0))
-                  ]),
-                )),
-            Card(
-                color: Colors.white,
+                            fontSize: 20.0)))),
+                          Card(
                 margin: const EdgeInsets.symmetric(
                     vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(children: [
-                    const Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    const SizedBox(width: 20.0),
-                    Text('yTataki@gmail.com',
+                child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal.shade900),
+                    title: Text('yaya@gmail.com',
                         style: TextStyle(
                             color: Colors.teal.shade900,
                             fontFamily: 'SourceSans3',
-                            fontSize: 20.0))
-                  ]),
-                ))
-          ]))),
+                            fontSize: 20.0))))
+                        ]))),
     );
   }
 }
